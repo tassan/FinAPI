5c3275bc492a3945a4817c6de6e02c3b72124577
Flávio Tassan <ftassan@outlook.com>
2022-03-13 12:23:02 -0300

    Statement and Deposit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { request } = require("express");
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidV4 } = require("uuid")
 
@@ -7,20 +9,28 @@ app.use(express.json());
 
 const customers = [];
 
-/*
- * cpf - string
- * name - string
- * id - UUID
- * statement []
- */
+// Middleware
+function verifyExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer)
+    return response.status(400).json({ error: "Customer not found"});
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
-    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf); 
+    const customerAlreadyExists = customers
+      .some((customer) => customer.cpf === cpf);
 
     if (customerAlreadyExists)
         return response.status(400).json({error: "Customer already exists!"});
-    
+
     customers.push({
         cpf,
         name,
@@ -29,6 +39,27 @@ app.post("/account", (request, response) => {
     });
 
     return response.status(201).send();
+});
+
+app.get("/statement", verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3000);
